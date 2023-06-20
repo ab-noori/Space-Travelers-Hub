d5ea988dc507fcc603dab58b4064abdb518c351c
til2to <padwusup@gmail.com>
2023-06-20 16:27:32 +0000

    remove nav items

--- src/components/Layout/navbar/navigationsLinks.js
+++ src/components/Layout/navbar/navigationsLinks.js
@@ -1,9 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const menuItems = [
-  { id: uuidv4(), path: '/', text: 'Rockets' },
-  { id: uuidv4(), path: 'missions', text: 'Missions' },
-  { id: uuidv4(), path: 'profile', text: 'Profile' },
+  { id: uuidv4(), path: '/', text: '' },
+  { id: uuidv4(), path: 'missions', text: '' },
+  { id: uuidv4(), path: 'profile', text: '' },
 ];
 
 export default menuItems;
