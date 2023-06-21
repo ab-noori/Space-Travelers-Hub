7e6d750e0076f694e3b73740f557e101b8a13b95
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-21 10:26:37 +0330

    Enhace the code for routing the pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import {
-  BrowserRouter as Router, Routes, Route,
-} from 'react-router-dom';
-
 import './App.scss';
 import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
-      <main>
-        <Router>
-          <Routes>
-            <Route path="/Space-Travelers-Hub/" element={<Layout />} />
-          </Routes>
-        </Router>
-      </main>
+      <Layout />
     </div>
   );
 }
--- src/components/Layout/navbar/navigationsLinks.js
+++ src/components/Layout/navbar/navigationsLinks.js
@@ -1,9 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const menuItems = [
-  { id: uuidv4(), path: '/', text: 'Rockets' },
-  { id: uuidv4(), path: 'missions', text: 'Missions' },
-  { id: uuidv4(), path: 'profile', text: 'MyProfile' },
+  { id: uuidv4(), path: '/Space-Travelers-Hub/', text: 'Rockets' },
+  { id: uuidv4(), path: '/Space-Travelers-Hub/missions', text: 'Missions' },
+  { id: uuidv4(), path: '/Space-Travelers-Hub/profile', text: 'MyProfile' },
 ];
 
 export default menuItems;
