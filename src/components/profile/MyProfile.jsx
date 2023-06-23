793e633e86d424fc3a5ecd0c110a007d655b1e8e
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-06-23 10:04:50 +0330

    Update the MyProfile component to display joint missions.

--- src/components/profile/MyProfile.jsx
+++ src/components/profile/MyProfile.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../../redux/missions/missionsSlice';
 import './MyProfile.scss';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const { missions, reservedMissions } = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  const reservedMissionsList = missions.filter((mission) => {
+    const res = reservedMissions.includes(mission.mission_id);
+    return res;
+  });
+
+  return (
+    <div>
+      <h2>My Joint Missions</h2>
+      <ul className="joint-missions-list">
+        {reservedMissionsList.map((mission) => (
+          <li key={mission.mission_id}>
+            {mission.mission_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default MyProfile;
